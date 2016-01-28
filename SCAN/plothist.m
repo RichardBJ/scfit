b9ca2d376d1cb27a60c0bd3c2cf094141ddfb722
Kevin Ogden <ogdenkev@gmail.com>
2016-01-28 07:31:23 -0500

    updates plothist.m to allow inputs for binwidth updates plot_2A_P552R to write a log file, and also save scaled pdf

--- SCAN/plothist.m
+++ SCAN/plothist.m
@@ -1,13 +1,26 @@
 function [ openvals, shutvals, xopen, xshut ] = plothist( dwells, states, varargin )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%PLOTHIST Summary of this function goes here
+%   INPUT
+%       dwells - a numerical vector (1-dimension) of the dwell times in
+%           milliseconds
+%       states - a numerical vector of same length as dwells giving the
+%           state of each dwell (should only have zeroes for closed and
+%           non-zeroes for open)
+%       obw (OPTIONAL) - open bin width in millisecs [default: 0.05]
+%       sbw (OPTIONAL) - shut bin width in millisecs [default: 0.1]
+%       hFig (OPTIONAL) - handle for plotting [default: [] ]
 
 p = inputParser;
 addRequired(p,'dwells',@isnumeric);
 addRequired(p,'states',@isnumeric);
+addOptional(p, 'obw', 0.05, @isnumeric);
+addOptional(p, 'sbw', 0.1, @isnumeric);
 validationFxn = @(x) numel(x)==2 && all(ishghandle(x));
 addOptional(p,'hFig',[],validationFxn);
 parse(p,dwells,states,varargin{:});
+
+obw = p.Results.obw;
+sbw = p.Results.sbw;
 
 opens = log10(dwells(states~=0));
 shuts = log10(dwells(states==0));
@@ -15,8 +28,10 @@ shuts = log10(dwells(states==0));
 % xopen = min(opens):0.05:max(opens);
 first = floor(10*min(opens))./10;
 last = ceil(10*max(opens))./10;
-xopen = first:0.1:last;
-xshut = min(shuts):0.1:max(shuts);
+% xopen = first:0.1:last;
+xopen = first:obw:last;
+% xshut = min(shuts):0.1:max(shuts);
+xshut = min(shuts):sbw:max(shuts);
 
 openvals = hist(opens,xopen);
 shutvals = hist(shuts,xshut);
--- plot_2A_P552R_open_hists_150813.m
+++ plot_2A_P552R_open_hists_150813.m
@@ -1,15 +1,51 @@
+% Creates the variable `fileName` in the base workspace
 [d, s] = dwtread;
-[rd, rs] = imposeres(d, s, 0.05, 0.05);
-redist_rd = smoothbinpdf(rd, 0.05, Inf);
-[opencts, shutcts, xopen, xshut] = plothist(redist_rd, rs);
-emdistfit(rd(rs == 1), [0.1 1], [0.3 0.7])
-% updated plothist sets 10 bins per decade, or a binwidth of 0.1
-AUC_open = 0.1*sum(opencts);
+[pthstr, name] = fileparts(fileName);
+
+% Log results
+logfname = fullfile(pthstr, [name, '.log']);
+logfile = fopen(logfname, 'w');
+fprintf(logfile, 'Read dwell times from %s\n', name);
+
+tres = 0.05;  % 50 microseconds
+fprintf(logfile, 'Imposing %f millisecond open and shut time resolution\n', tres);
+[rd, rs] = imposeres(d, s, tres, tres);
+
+sampling_int = 0.025;
+fprintf(logfile, 'Smoothing the pdf with sampling interval %f\n', sampling_int);
+redist_rd = smoothbinpdf(rd, sampling_int, Inf);
+
+fprintf(logfile, 'Plotting histograms\n-------------------------\n');
+openbinwidth = 0.05;
+shutbinwidth = 0.1;
+fprintf(logfile, 'Open binwidth: %f\nShut binwidth: %f\n', openbinwidth, shutbinwidth);
+[opencts, shutcts, xopen, xshut] = plothist(redist_rd, rs, openbinwidth, shutbinwidth);
+
+fprintf('Fitting open time histogram\n');
+[ opentaus, openareas, ~, Nopens, ~, ~, ~, ~, ~, ~, ~, ~, ...
+    pdfxlog, flog, ~, ~] = emdistfit(rd(rs == 1), [0.1 1], [0.3 0.7]);
+
+AUC_open = openbinwidth*sum(opencts);
 % AUC_open = 0.05*sum(opencts); % 0.05 binwidth from plothist
 opencts = opencts / AUC_open;
-% for shut times, I think the binwidth in plothist is 0.1, not 0.05
-AUC_shut = 0.05*sum(shutcts);
+AUC_shut = shutbinwidth*sum(shutcts);
 shutcts = shutcts / AUC_shut;
-[pthstr, name] = fileparts(fileName);
-csvwrite(fullfile(pthstr, [name, '.opens']), [xopen', opencts']);
-csvwrite(fullfile(pthstr, [name, '.shuts']), [xshut', shutcts']);
+
+opencts_out = fullfile(pthstr, [name, '.opens']);
+fprintf('Saving smooth open counts in %s\n', opencts_out);
+csvwrite(opencts_out, [xopen', opencts']);
+
+openfit_out = fullfile(pthstr, [name, '.ofit']);
+fprintf('Saving exp fit for plotting to %s\n', openfit_out);
+% scale pdf for the effective number of openings
+openpdf = Nopens*openbinwidth*flog;
+% now scale pdf to match the 'normalize' histogram
+openpdf = openpdf / AUC_open;
+csvwrite(openfit_out, [pdfxlog', openpdf']);
+
+% Do not save shut times because
+% 1 - they are meaningless for 2A(P552R) recordings
+% 2 - need to verify bin width in plothist for shut times
+% csvwrite(fullfile(pthstr, [name, '.shuts']), [xshut', shutcts']);
+
+fclose(logfile);
