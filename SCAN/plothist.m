0f3a34807123e78a307cd018b958fbfd0d92f141
Kevin Ogden <ogdenkev@gmail.com>
2017-11-29 00:50:18 -0500

    Updates plothist.m to use axes handles (in addition to figure handles)
    This change allows plothist to work with the SCFit.mlapp application

--- SCAN/plothist.m
+++ SCAN/plothist.m
@@ -9,6 +9,8 @@ function [ openvals, shutvals, xopen, xshut ] = plothist( dwells, states, vararg
 %       obw (OPTIONAL) - open bin width in millisecs [default: 0.05]
 %       sbw (OPTIONAL) - shut bin width in millisecs [default: 0.1]
 %       hFig (OPTIONAL) - handle for plotting [default: [] ]
+%       axes (OPTIONAL) - logical indicating whether hFig contains axis
+%       handles or figure handles
 
 p = inputParser;
 addRequired(p,'dwells',@isnumeric);
@@ -17,6 +19,7 @@ addOptional(p, 'obw', 0.05, @isnumeric);
 addOptional(p, 'sbw', 0.1, @isnumeric);
 validationFxn = @(x) numel(x)==2 && all(ishghandle(x));
 addOptional(p,'hFig',[],validationFxn);
+addOptional(p, 'axes', false, @islogical);
 parse(p,dwells,states,varargin{:});
 
 obw = p.Results.obw;
@@ -38,21 +41,34 @@ shutvals = hist(shuts,xshut);
 
 if isempty(p.Results.hFig)
     figure;
-else
+elseif ~p.Results.axes
     figure(p.Results.hFig(1));
 end
-bar(xopen,sqrt(openvals),1);
-xlabel('Log [open times (ms)]');
-ylabel('Count (square root scale)');
+if p.Results.axes && ~isempty(p.Results.hFig)
+    bar(p.Results.hFig(1), xopen, sqrt(openvals), 1);
+    xlabel(p.Results.hFig(1), 'Log [open times (ms)]');
+    ylabel(p.Results.hFig(1), 'Count (square root scale)');
+else
+    bar(xopen,sqrt(openvals),1);
+    xlabel('Log [open times (ms)]');
+    ylabel('Count (square root scale)');
+end
+
 
 if isempty(p.Results.hFig)
     figure;
-else
+elseif ~p.Results.axes
     figure(p.Results.hFig(2));
 end
-bar(xshut,sqrt(shutvals),1);
-xlabel('Log [shut times (ms)]');
-ylabel('Count (square root scale)');
+if ~p.Results.axes
+    bar(xshut,sqrt(shutvals),1);
+    xlabel('Log [shut times (ms)]');
+    ylabel('Count (square root scale)');
+elseif ~isempty(p.Results.hFig)
+    bar(p.Results.hFig(2), xshut, sqrt(shutvals), 1);
+    xlabel(p.Results.hFig(2), 'Log [shut time (ms)]');
+    ylabel(p.Results.hFig(2), 'Count (square root scale)');
+end
 
 end
 
